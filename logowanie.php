205a9eaab19b71c6b8a031c675597d661df59622
johny pierony <90196123+Wszechmogancy@users.noreply.github.com>
2022-01-25 20:27:27 +0100

    małe zmiany i dodanie pliku php

--- logowanie.php
+++ logowanie.php
@@ -1,10 +1,9 @@
 <?php
-$uzytkownik = "j0ny";
-$haslo = "maslo123";
-$login_u = $_GET['login'];
-$haslo_u = $_GET['haslo'];
 
-if($login_u=== $uzytkownik and $haslo_u===$haslo){
-    echo "zalogowano";
-};
+require_once"connect.php";
+
+$login_u = $_POST['login'];
+$haslo_u = $_POST['haslo'];
+
+
 ?>
